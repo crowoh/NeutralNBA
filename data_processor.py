004224c220b148fe2d2931f0190aaea1458de0fd
Cooper Rubens <crowohh@gmail.com>
2024-08-05 15:59:42 -0700

    moved array
    2 big

--- data_processor.py
+++ data_processor.py
@@ -3,44 +3,15 @@ import numpy as np
 from nba_api.stats.endpoints import playergamelog, teamgamelog, commonplayerinfo
 from nba_api.stats.static import teams
 from datetime import datetime
+from nba_coords import nba_arenas_coordinates as arenas
+
 
 # Constants for calculations
 AVG_FUEL_CONSUMPTION_PER_KM = 0.36  # gallons per kilometer
 KG_CO2_PER_GALLON_JET_FUEL = 9.57  # kg CO2 emissions per gallon of jet fuel
 COST_PER_GALLON_JET_FUEL = 2.22  # cost per gallon of jet fuel in dollars
 
-nba_arenas_coordinates = {
-    'ATL': (33.7573, -84.3963),  # Atlanta Hawks - State Farm Arena
-    'BOS': (42.3662, -71.0621),  # Boston Celtics - TD Garden
-    'BKN': (40.6826, -73.9745),  # Brooklyn Nets - Barclays Center
-    'CHA': (35.2255, -80.8390),  # Charlotte Hornets - Spectrum Center
-    'CHI': (41.8807, -87.6742),  # Chicago Bulls - United Center
-    'CLE': (41.4965, -81.6882),  # Cleveland Cavaliers - Rocket Mortgage FieldHouse
-    'DAL': (32.7902, -96.8102),  # Dallas Mavericks - American Airlines Center
-    'DEN': (39.7487, -105.0077),  # Denver Nuggets - Ball Arena
-    'DET': (42.3411, -83.0553),  # Detroit Pistons - Little Caesars Arena
-    'GSW': (37.7680, -122.3878),  # Golden State Warriors - Chase Center
-    'HOU': (29.7508, -95.3621),  # Houston Rockets - Toyota Center
-    'IND': (39.7631, -86.1555),  # Indiana Pacers - Gainbridge Fieldhouse
-    'LAC': (34.0430, -118.2673),  # LA Clippers - Crypto.com Arena
-    'LAL': (34.0430, -118.2673),  # LA Lakers - Crypto.com Arena
-    'MEM': (35.1380, -90.0506),  # Memphis Grizzlies - FedExForum
-    'MIA': (25.7814, -80.1866),  # Miami Heat - FTX Arena
-    'MIL': (43.0436, -87.9169),  # Milwaukee Bucks - Fiserv Forum
-    'MIN': (44.9795, -93.2761),  # Minnesota Timberwolves - Target Center
-    'NOP': (29.9489, -90.0811),  # New Orleans Pelicans - Smoothie King Center
-    'NYK': (40.7505, -73.9934),  # New York Knicks - Madison Square Garden
-    'OKC': (35.4634, -97.5151),  # Oklahoma City Thunder - Paycom Center
-    'ORL': (28.5392, -81.3838),  # Orlando Magic - Amway Center
-    'PHI': (39.9018, -75.1714),  # Philadelphia 76ers - Wells Fargo Center
-    'PHX': (33.4457, -112.0712),  # Phoenix Suns - Footprint Center
-    'POR': (45.5315, -122.6664),  # Portland Trail Blazers - Moda Center
-    'SAC': (38.5802, -121.4994),  # Sacramento Kings - Golden 1 Center
-    'SAS': (29.4268, -98.4375),  # San Antonio Spurs - AT&T Center
-    'TOR': (43.6435, -79.3791),  # Toronto Raptors - Scotiabank Arena
-    'UTA': (40.7683, -111.9011),  # Utah Jazz - Vivint Arena
-    'WAS': (38.8981, -77.0209),  # Washington Wizards - Capital One Arena
-}
+nba_arenas_coordinates = arenas
 
 def parse_date(date_string):
     try:
